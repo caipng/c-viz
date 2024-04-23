807eb9d184eb63e2b8318e11a52a8e5c4621a653
Cai <caipngg@gmail.com>
2024-04-23 22:36:39 +0800

    fix initialized check for structs

--- src/interpreter/initializedTable.ts
+++ src/interpreter/initializedTable.ts
@@ -1,25 +1,32 @@
 import { cloneDeep } from "lodash";
-import { ObjectTypeInfo } from "../typing/types";
+import { ObjectTypeInfo, isStructure } from "../typing/types";
 
 export class InitializedTable {
-    private table: Record<number, boolean> = {};
+  private table: Record<number, boolean> = {};
 
-    public add(addr: number, t: ObjectTypeInfo): void {
-        for (let i = addr; i < addr + t.size; i++) this.table[i] = true;
-    }
+  public add(addr: number, t: ObjectTypeInfo): void {
+    for (let i = addr; i < addr + t.size; i++) this.table[i] = true;
+  }
 
-    public check(addr: number, t: ObjectTypeInfo): boolean {
-        for (let i = addr; i < addr + t.size; i++) if (!(i in this.table)) return false;
-        return true;
+  public check(addr: number, t: ObjectTypeInfo): boolean {
+    if (isStructure(t)) {
+      return t.members.reduce(
+        (A, i) => A && this.check(addr + i.relativeAddress, i.type),
+        true,
+      );
     }
+    for (let i = addr; i < addr + t.size; i++)
+      if (!(i in this.table)) return false;
+    return true;
+  }
 
-    public remove(addr: number, size: number): void {
-        for (let i = addr; i < addr + size; i++) {
-            if (i in this.table) delete this.table[i]
-        }
+  public remove(addr: number, size: number): void {
+    for (let i = addr; i < addr + size; i++) {
+      if (i in this.table) delete this.table[i];
     }
+  }
 
-    public getTable() {
-        return cloneDeep(this.table)
-    }
+  public getTable() {
+    return cloneDeep(this.table);
+  }
 }
