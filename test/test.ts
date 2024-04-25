f68c6a8d2d368db06f25c04423283ae758eb142c
Cai <caipngg@gmail.com>
2024-04-26 00:58:25 +0800

    fix tests

--- test/test.ts
+++ test/test.ts
@@ -1,18 +1,18 @@
 import { load } from "js-yaml";
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync } from "fs";
 import cviz from "../src/index";
-import { typeTranslationUnit } from "../src/typing/main";
 import { assert } from "chai";
 import { describe, it } from "mocha";
 import "../src/types";
 
 const TEST_FOLDER_PATH = "./test/";
-const TEST_OUTPUT_PATH = TEST_FOLDER_PATH + "out/";
+// const TEST_OUTPUT_PATH = TEST_FOLDER_PATH + "out/";
 
 interface TestCase {
   in?: string;
   out?: string;
   desc?: string;
+  fail?: boolean;
 }
 
 interface TestSuite {
@@ -34,24 +34,34 @@ const tests = load(
   readFileSync(TEST_FOLDER_PATH + "test.yaml", "utf-8"),
 ) as TestList;
 
+const run = (source: string): number => {
+  const rt = cviz.run(source)
+  while (rt.exitCode === undefined) rt.next();
+  return rt.exitCode;
+}
+
 for (const [testName, testSuite] of Object.entries(tests)) {
   describe(testName, () => {
     const source = readFileSync(TEST_FOLDER_PATH + testSuite.file, "utf-8");
 
     for (let i = 0; i < testSuite.cases.length; i++) {
       const testCase = testSuite.cases[i];
-      // const expectedOutput = testCase.out;
+      const expectedOutput = testCase.out;
+      const toFail = testCase.fail === true;
       let caseName = "Case " + i;
       if (testCase.desc) caseName += " (" + testCase.desc + ")";
 
       it(caseName, () => {
-        const out = typeTranslationUnit(cviz.parseProgram(source));
-        writeFileSync(
-          TEST_OUTPUT_PATH + testName + i + ".json",
-          JSON.stringify(out),
-        );
-        // assert.equal(out, expectedOutput);
-        assert.equal(1, 1);
+        if (toFail) {
+          assert.throws(() => run(source), Error) 
+        } else {
+          const out = run(source);
+          // writeFileSync(
+          //   TEST_OUTPUT_PATH + testName + i + ".json",
+          //   JSON.stringify(out),
+          // );
+          assert.equal(out.toString(), expectedOutput);
+        }
       });
     }
   });
